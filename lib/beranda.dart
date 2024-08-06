e44f324d337a0812d55b1facd625da75f9f24948
Reizqysukadesain <reizqymarhas@gmail.com>
2024-08-06 12:19:51 +0700

    stututututu

---
+++ lib/beranda.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class beranda extends StatefulWidget {
+  const beranda({super.key});
+
+  @override
+  State<beranda> createState() => _berandaState();
+}
+
+class _berandaState extends State<beranda> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(children: [Text("Halo"), Text("saya"), Text("sedang"), Text("belajar"), Text("flutter")],)
+    );
+  }
+}
